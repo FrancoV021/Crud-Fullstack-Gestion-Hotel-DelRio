0dcc70a409aff01bfd919ca43fc24b4e831d58e5
Franco Santacatalina. <Franhi02@hotmail.com>
2026-02-06 12:11:15 -0300

    Auth fixs

--- backend/src/main/java/com/backend/backend/config/CorsConfig.java
+++ backend/src/main/java/com/backend/backend/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
 
